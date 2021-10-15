ae70841ff600d41801309df3e8c5dec0dc355bf5
Ismael Marin <ismael.marin@gmail.com>
2021-10-15 13:59:53 -0500

    File validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,11 @@
+# frozen_string_literal: true
+
 class Comment < ApplicationRecord
   belongs_to :user
   has_many_attached :files
+
+  validates :files, attached: true,
+                    limit: { min: 1, max: 3 },
+                    content_type: { in: 'text/plain',
+                                    message: 'the file type needs to be text/plain' }
 end
